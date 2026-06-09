c7e00110e06d31df992cc72c5ca5fb08427a989b
kimgusxokim <fungee2009@naver.com>
2026-06-09 10:02:46 +0900

    28:51

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <header>
+      <nav>
+        <img src="/logo.svg" alt="Apple Logo" />
+        <ul>
+          {[{ label: "Store" }, { label: "Mac" }, { label: "iPhone" }, { label: "Watch" }, { label: "Vision" }, { label: "Airpods" }].map((link) => (
+            <li key={link.label}>
+              <a href="{link.label}">{link.label}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="flex-center gap-3">
+          <button>
+            <img src="/search.svg" alt="Search" />
+          </button>
+          <button>
+            <img src="/cart.svg" alt="Cart" />
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
