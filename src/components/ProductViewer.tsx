7747a543ed55f1179130e85cfedcba67f39ced69
kimgusxokim <fungee2009@naver.com>
2026-06-09 14:15:55 +0900

    1:12:28

--- src/components/ProductViewer.tsx
+++ src/components/ProductViewer.tsx
@@ -15,10 +15,6 @@ const ProductViewer = () => {
     <section id="product-viewer">
       <h2>Take a closer look.</h2>
       <div className="controls">
-        <p className="info">
-          MacbookPro {scale} in {color}
-        </p>
-
         <div className="flex-center gap-5 mt-5">
           <div className="color-control">
             <div onClick={() => setColor("#adb5bd")} className={clsx("bg-neutral-300", color === "#adb5bd" && "active")} />
